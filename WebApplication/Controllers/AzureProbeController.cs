128baec6468c9260034a566b6512c29d78ac706a
Hans Kindberg <hans.kindberg@gmail.com>
2017-03-09 08:24:50 +0100

    Azure-deployment stuff

---
+++ WebApplication/Controllers/AzureProbeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using WebApplication.Models.ViewModels;
+
+namespace WebApplication.Controllers
+{
+	public class AzureProbeController : SiteController
+	{
+		#region Methods
+
+		public virtual ActionResult Index()
+		{
+			return this.View(new AzureProbeViewModel());
+		}
+
+		#endregion
+	}
+}
